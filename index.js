6b26b5b618753c438aa78e5e55a3ce7de405c237
Niklas Bratt <niklas.bratt@consid.se>
2021-01-07 17:21:41 +0100

    Added webhook for Strava

--- index.js
+++ index.js
@@ -411,6 +411,33 @@ app.get('/api/v1/strava/activities', (req, res) => {
     .catch( err => res.send([]));
 
 });
+
+// Creates the endpoint for our webhook
+app.post('/stravaWebhook', (req, res) => {
+    console.log("webhook event received!", req.query, req.body);
+    res.status(200).send('EVENT_RECEIVED');
+});
+// Adds support for GET requests to our webhook
+app.get('/stravaWebhook', (req, res) => {
+    // Your verify token. Should be a random string.
+    const VERIFY_TOKEN = "niklaskingstravatrbok";
+    // Parses the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Verifies that the mode and token sent are valid
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {     
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.json({"hub.challenge":challenge});  
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+});
 app.get('/', (req, res) => {
   res.send('Hello Kingen!');
 });
