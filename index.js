ab3c13c288203d699459fe4f7ba17a788c2b8a4a
Niklas Bratt <niklas.bratt@consid.se>
2021-01-08 08:14:09 +0100

    .

--- index.js
+++ index.js
@@ -434,9 +434,9 @@ app.post('/stravaWebhook', async (req, res) => {
             const accessToken = await authorize(req.body.owner_id);
             console.log('**Authorized: ' + accessToken);
             const result = await getStravaActivity(accessToken, activityId);
-            console.log('Result: ' + result);
-            if (result.length > 0) {
-                const item = result[0];
+            console.log('Result: ' + result.data);
+            if (result.data.length > 0) {
+                const item = result.data[0];
 
                 const startTime = moment(item.start_date).format('HH:mm');
                 const lsd = item.moving_time > 5400 ? 1 : 0;
