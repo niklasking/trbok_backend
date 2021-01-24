a6841660e97b97faabf8788b6b0a515305a2e41c
Niklas Bratt <niklas.bratt@consid.se>
2021-01-24 18:46:16 +0100

    .

--- index.js
+++ index.js
@@ -494,8 +494,8 @@ app.get('/api/v1/strava/activities/between', async (req, res) => {
         result.map( item => {
             try {
                 // Get laps
-//                const laps = await getStravaLaps(item.id);
-//                console.log(laps);
+                const laps = await getStravaLaps(item.id);
+                console.log(laps);
                 // Save activity
                 const startTime = moment(item.start_date).format('HH:mm');
                 const lsd = item.moving_time > 5400 ? 1 : 0;
