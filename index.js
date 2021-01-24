e136e98115c27f3b8eed9cbec7bbe04e10428392
Niklas Bratt <niklas.bratt@consid.se>
2021-01-24 19:07:10 +0100

    .

--- index.js
+++ index.js
@@ -538,7 +538,7 @@ app.get('/api/v1/strava/activities/between', async (req, res) => {
 //                    laps: [laps]
                 }
             );
-            activity.save();
+            await activity.save();
         });
         res.status(200).send('Ok');
     }
