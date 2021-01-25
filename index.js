9aec37f6c8432d86ffe44c3cb0c144a5413af7d1
Niklas Bratt <niklas.bratt@consid.se>
2021-01-25 14:11:30 +0100

    .

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ getStravaStreams = async (accessToken, activityId) => {
         console.log('**************');
 */
         let response = await axios.get('https://www.strava.com/api/v3/activities/' + activityId + '/streams' +
-                                        '?keys=[heartrate]&key_by_type=true', {
+                                        '?keys=[time, distance, latlng, altitude, velocity_smooth, heartrate, cadence, watts, temp, moving, grade_smooth]&key_by_type=true', {
             headers: {
                 Authorization: 'Bearer ' + accessToken
             }
